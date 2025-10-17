55c4de984ca0f2035250639521b78992bf6f63a0
sujichowdary2003-cmd <sujichowdary2003@gmail.com>
2025-10-17 15:30:08 +0530

    Update app_Version2.py

--- app_Version2.py
+++ app_Version2.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import RandomForest
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import joblib
